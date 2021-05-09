ebc6fbc24405d96dfffd8977129c9b2eefcefc39
prakashios <skoruz@skoruz-mac.local>
2021-05-09 17:06:41 +0530

    home page image added

---
+++ components/layout.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import Head from 'next/head';
+// import Header from './header.js';
+
+class layout extends Component {
+    render() {
+        return (
+            <div>
+                <Head>
+                    <title>RESUME</title>
+                    <link rel="icon" href="/favicon.ico" />
+                    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous" />
+                    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0"/>
+                </Head>
+                {/* <Header/> */}
+                <div className="content-wrap">
+                    {this.props.children}
+                </div>
+                <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+                <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+                <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.min.js" integrity="sha384-+YQ4JLhjyBLPDQt//I+STsc9iw4uQqACwlvpslubQzn4u2UU2UFM80nGisd026JF" crossorigin="anonymous"></script>
+            </div>
+        );
+    }
+}
+
+export default layout;
